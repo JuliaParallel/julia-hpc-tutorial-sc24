b7ee8c5ec2fa7faf6718cb9650660f67d305ca59
Johannes Blaschke <jpblaschke@lbl.gov>
2024-11-04 08:59:11 -0800

    add basic hello-world mpi jobscript

---
+++ parts/hpc/solution/hello_world.jl
@@ -0,0 +1,14 @@
+using MPI
+
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
+name = gethostname()
+
+print("Hello world, I am rank $(rank) of $(size) on $(name)\n")
+
+MPI.Barrier(comm)
+
+if rank == 0
+    print(MPI.versioninfo())
+end
---
+++ parts/hpc/solution/job_cpu_singlenode.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -A ntrain1
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH --output=slurm_cpu_singlenode.out
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+
+# Load the latest Julia Module
+ml load julia
+
+# this will load the activate.sh in the root path of this repository -- the
+# __job_script_dir points to the location of _this_ file in the file system (
+# the bash invocation is a bit of arcane code to convert bash_source to a
+# reasonable path on the file system). things like this are not strictly
+# necessary, but they reduce the likelihood of mishaps (eg. forgetting to
+# source activate.sh, or running this script from a different working
+# directory)
+__job_script_dir=$(
+    cd -- "$( dirname -- "${bash_source[0]}" )" &> /dev/null && pwd
+)
+# NOTE: because the `solution folder is at a deeper level from the project root
+# as the excercise, we've added an additional `/..` to get back to the
+# `activate.sh`
+source ${__job_script_dir}/../../../activate.sh
+
+# Run the Julia code -- we're usign `srun` to launch Julia. This is necessary
+# to configure MPI. If you tried to use `MPI.Init()` outside of an srun, then
+# the program will crash. Note also that you can't run an srun _insite_ of
+# another srun.
+
+# NOTE: when you specify --ntasks or --ntasks-per-node in the jobscript header,
+# then srun inherits these. The command below is equivalent to: `srun -n 4`
+srun julia hello_world.jl
